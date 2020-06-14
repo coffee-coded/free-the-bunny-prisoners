8648303c14b1df24d95098f9148225f10d1bfccc
Anish Bhattacharya <z3xt5r@gmail.com>
2020-06-14 21:38:36 +0530

    Successful

--- main.py
+++ main.py
@@ -9,15 +9,4 @@ def solution(num_buns, num_required):
         for bunny in bunnies:
             print(keyrings, bunny, key, bunnies)
             keyrings[bunny].append(key)
-
     return keyrings
-
-
-if __name__ == "__main__":
-    # assert all([all([x == y for x, y in zip(a, b)])
-    #             for a, b in zip([[0, 1, 2, 3, 4, 5], [0, 1, 2, 6, 7, 8], [0, 3, 4, 6, 7, 9], [1, 3, 5, 6, 8, 9], [2, 4, 5, 7, 8, 9]], solution(5, 3))])
-    # assert all([all([x == y for x, y in zip(a, b)])
-    #             for a, b in zip([[0], [1], [2], [3]], solution(4, 4))])
-
-    # print(solution(4, 4))
-    print(solution(5, 3))
